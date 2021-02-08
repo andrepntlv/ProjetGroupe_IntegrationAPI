d679447beb81d7be4678610d5898b21c035205ca
Pierre_45 <elv-assagnon.km@eduge.ch>
2021-02-08 11:30:32 +0100

    Création de la vue + page accueil

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+import finnhub
+# Setup client
+finnhub_client = finnhub.Client(api_key="c0db36n48v6vf7f7l8mg")
 # Create your views here.
 def home(request):
-    return render(request, 'home/accueil.html')
+    listeAction = (finnhub_client.stock_symbols('US')[0:10])
+    cle=listeAction[0]
+
+    context={
+        "listeAction":listeAction,
+        "cle":cle,
+    }
+    return render(request, 'home/accueil.html',context)
